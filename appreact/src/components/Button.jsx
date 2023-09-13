c1734e50b1e4f6d97647cfd7095cbcabc87603f0
NinaGoislot <ninagoislot.work@gmail.com>
2023-09-13 06:02:47 +0200

    Mise en place du composant Button

---
+++ appreact/src/components/Button.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas);
+
+function Button({ icon, link, label, title, onClick }) {
+
+    const selectedIcon = icon ? fas[icon] : null;
+    const buttonTitle = title || label;
+
+    const handleClick = () => {
+        if (onClick) {
+            onClick();
+        }
+    };
+
+    return (
+
+        <button
+            className={`w-10 h-10 text-sm font-semibold bg-sky-300 text-[#3A2D2E] hover:bg-[#B69288] hover:text-[#F9EEE4]  rounded transition duration-00 transform`}
+            title={buttonTitle}
+            onClick={handleClick}>
+
+            <Link to={link}>
+                {label && <span>{label}</span>}
+                {selectedIcon && (
+                    <FontAwesomeIcon
+                        icon={selectedIcon}
+                        className={label ? 'ml-1' : ''}
+                    />
+                )}
+            </Link>
+        </button>
+    )
+}
+
+export default Button;
